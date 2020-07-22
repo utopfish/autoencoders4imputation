d72d6527e7bba0c7fad99a716462518bba7b329a
liumeng <utopfish@163.com>
2020-07-22 23:04:01 +0800

    加入三种缺失模式，还在进行实验，在鸢尾花数据集上初步效果不太好

---
+++ test/test_baseline_imputation_true.py
@@ -0,0 +1,106 @@
+"""
+@author: liuAmon
+@contact:utopfish@163.com
+@file: test_baseline_imputation_true.py
+@time: 2020/7/22 20:04
+"""
+import os
+import impyute
+import numpy as np
+import pandas as pd
+
+from logger import logger
+from ycimpute.utils import evaluate
+from matplotlib import pyplot as plt
+from utils.handle_missingdata import gene_missingdata,gene_missingdata_taxa_bias,gene_missingdata_chara_bias,gene_missingdata_block_bias
+from dnn.mida import MIDA
+from dnn.gain import GAIN
+from ycimpute.imputer import knnimput, mice, EM
+from fancyimpute import KNN, NuclearNormMinimization, SoftImpute, IterativeImputer, BiScaler, SimpleFill
+
+path = r'../public_data/'
+pciturePath = r'G:\labWork\imputation_plt\pub'
+for file in os.listdir(path):
+    logger.info("**********************{}********************".format(file))
+    data = pd.read_excel(os.path.join(path, file), sheet_name="dataset")
+    dt = np.array(data.values)
+    data = dt.astype('float')
+    origin_data = data[:, :-1]
+    target = data[:, -1]
+
+    for miss_pat in ['normal','taxa','chara','block']:
+        mice_rmse = []
+        ii_rmse = []
+        median_rmse = []
+        random_rmse = []
+        mida_rmse = []
+        gain_rmse = []
+        for i in [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+            if miss_pat=='normal':
+                miss_data = gene_missingdata(rate=i, data=origin_data)
+            elif miss_pat=='taxa':
+                miss_data =gene_missingdata_taxa_bias(rate=i, data=origin_data)
+            elif miss_pat =='chara':
+                miss_data =gene_missingdata_chara_bias(rate=i, data=origin_data)
+            elif miss_pat== 'block':
+                miss_data=gene_missingdata_block_bias(rate=i, data=origin_data)
+            else:
+                raise Exception("缺失模式错误，请在'normal','taxa','chara','block'中选择对应模式")
+            try:
+                imputed_data = mice.MICE().complete(miss_data)
+                score = evaluate.RMSE(origin_data, imputed_data)
+                mice_rmse.append(score)
+                logger.info("MICE missing rate:{},RMSE:{}".format(i, score))
+            except:
+                mice_rmse.append(np.nan)
+            try:
+                imputed_data = IterativeImputer().fit_transform(miss_data)
+                score = evaluate.RMSE(origin_data, imputed_data)
+                ii_rmse.append(score)
+                logger.info("fi IterativeImputer missing rate:{},RMSE:{}".format(i, score))
+            except:
+                ii_rmse.append(np.nan)
+            try:
+                imputed_data = SimpleFill("median").fit_transform(miss_data)
+                score = evaluate.RMSE(origin_data, imputed_data)
+                median_rmse.append(score)
+                logger.info("fi median missing rate:{},RMSE:{}".format(i, score))
+            except:
+                median_rmse.append(np.nan)
+            try:
+                imputed_data = impyute.imputation.cs.random(miss_data)
+                score = evaluate.RMSE(origin_data, imputed_data)
+                random_rmse.append(score)
+                logger.info("random missing rate:{},RMSE:{}".format(i, score))
+            except:
+                random_rmse.append(np.nan)
+            try:
+                imputed_data = MIDA().complete(miss_data)
+                score = evaluate.RMSE(origin_data, imputed_data)
+                logger.info("MIDA missing rate:{},RMSE:{}".format(i, score))
+                mida_rmse.append(score)
+            except:
+                mida_rmse.append(np.nan)
+            try:
+                imputed_data = GAIN().complete(miss_data)
+                score = evaluate.RMSE(origin_data, imputed_data)
+                logger.info("MIDA missing rate:{},RMSE:{}".format(i, score))
+                gain_rmse.append(score)
+            except:
+                gain_rmse.append(np.nan)
+
+        color = ['blue', 'green', 'red', 'yellow', 'black', 'burlywood', 'cadetblue', 'chartreuse', 'purple', 'coral',
+                 'aqua', 'aquamarine', 'darkblue', 'y','m']
+
+        plt.figure()
+        x = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]
+        plt.plot(x, mice_rmse, color=color[1], label='mice')
+        plt.plot(x, ii_rmse, color=color[2], label='ii')
+        plt.plot(x, median_rmse, color=color[3], linestyle=':',label='median')
+        plt.plot(x, random_rmse, color=color[9], linestyle=':',label='random')
+        plt.plot(x, mida_rmse, color=color[13], linewidth=3.0,linestyle='-.',label='mida')
+        plt.plot(x, gain_rmse, color=color[14], linewidth=3.0, linestyle='-.', label='gain')
+        plt.title("rmse of different missing rate in {}_{}".format(file,miss_pat))
+        plt.legend(loc="upper left")
+        # plt.show()
+        plt.savefig(os.path.join(pciturePath, "rmse_of_different_missing_rate_in_{}_{}.png".format(file,miss_pat)))
--- utils/handle_missingdata.py
+++ utils/handle_missingdata.py
@@ -19,33 +19,101 @@ def gene_missingdata(rate,data,seed=0):
     随机模式缺失
     :return:缺失后数据集和缺失行数
     '''
-    # random.seed=seed
-    count=0
+    random.seed=seed
     miss_data=data.copy()
-    while count<rate*len(miss_data)*len(miss_data[0]):
-        row=int(random.random()*len(miss_data))
-        col=int(random.random()*len(miss_data[0]))
-        if miss_data[row][col]!=np.nan:
-            miss_data[row][col]=np.nan
-            count+=1
+    nrow, ncol = miss_data.shape
+    myList = list(np.linspace(0, ncol*nrow-1, ncol*nrow, dtype=int))
+    choice_num=random.sample(myList,int(ncol*nrow*rate))
+    for num in choice_num:
+        row=num%nrow
+        col=num//nrow
+        miss_data[row][col] = np.nan
     return miss_data
+def gene_missingdata_taxa_bias(rate,data,seed=0):
+    random.seed=seed
+    miss_data=data.copy()
+    nrow, ncol = miss_data.shape
+    choice_row=random.sample(list(np.linspace(0, nrow-1, nrow, dtype=int)),int(nrow*rate))
+    for row in choice_row:
+        choice_col=random.sample(list(np.linspace(0, ncol-1, ncol, dtype=int)),int(ncol*rate))
+        for col in choice_col:
+            miss_data[row][col]=np.nan
+    return miss_data
+def gene_missingdata_chara_bias(rate,data,seed=0):
+    random.seed=seed
+    miss_data=data.copy()
+    nrow, ncol = miss_data.shape
+    choice_col = random.sample(list(np.linspace(0, ncol - 1, ncol, dtype=int)), int(ncol * rate))
+    for col in choice_col:
+        choice_row=random.sample(list(np.linspace(0, nrow-1, nrow, dtype=int)),int(nrow*rate))
+        for row in choice_row:
+            miss_data[row][col]=np.nan
+    return miss_data
+def gene_missingdata_block_bias(rate,data,seed=0):
+    random.seed=seed
+    miss_data=data.copy()
+    nrow, ncol = miss_data.shape
+    rowlength=random.uniform(rate,1)
+    collength=rate/rowlength
+    rowlength=round(rowlength*nrow)
+    collength=round(collength*ncol)
+    start_row=int(random.random()*nrow)
+    start_col=int(random.random()*ncol)
+    for row in np.arange(0,rowlength):
+        for col in np.arange(0,collength):
+            miss_data[(start_row+row)%nrow][(start_col+col)%ncol] = np.nan
+    return miss_data
+
 def gene_missingdata_df(rate,data,seed=0):
     '''
     :param rate: 缺失率，int
-    :param data: 完整数据集，df
+    :param data: 完整数据集，np.array
     随机模式缺失
     :return:缺失后数据集和缺失行数
     '''
     random.seed=seed
-    count=0
     miss_data=data.copy()
-    nrow,ncol=miss_data.shape
-    while count<rate*nrow*ncol:
-        row=int(random.random()*nrow)
-        col=int(random.random()*ncol)
-        if miss_data.iloc[row,col]!=np.nan:
+    nrow, ncol = miss_data.shape
+    myList = list(np.linspace(0, ncol*nrow-1, ncol*nrow, dtype=int))
+    choice_num=random.sample(myList,int(ncol*nrow*rate))
+    for num in choice_num:
+        row=num%nrow
+        col=num//nrow
+        miss_data.iloc[row,col] = np.nan
+    return miss_data
+def gene_missingdata_taxa_bias_df(rate,data,seed=0):
+    random.seed=seed
+    miss_data=data.copy()
+    nrow, ncol = miss_data.shape
+    choice_row=random.sample(list(np.linspace(0, nrow-1, nrow, dtype=int)),int(nrow*rate))
+    for row in choice_row:
+        choice_col=random.sample(list(np.linspace(0, ncol-1, ncol, dtype=int)),int(ncol*rate))
+        for col in choice_col:
             miss_data.iloc[row,col]=np.nan
-            count+=1
+    return miss_data
+def gene_missingdata_chara_bias_df(rate,data,seed=0):
+    random.seed=seed
+    miss_data=data.copy()
+    nrow, ncol = miss_data.shape
+    choice_col = random.sample(list(np.linspace(0, ncol - 1, ncol, dtype=int)), int(ncol * rate))
+    for row in choice_col:
+        choice_row=random.sample(list(np.linspace(0, nrow-1, nrow, dtype=int)),int(nrow*rate))
+        for col in choice_row:
+            miss_data.iloc[row,col]=np.nan
+    return miss_data
+def gene_missingdata_block_bias_df(rate,data,seed=0):
+    random.seed=seed
+    miss_data=data.copy()
+    nrow, ncol = miss_data.shape
+    rowlength=random.uniform(rate,1)
+    collength=rate/rowlength
+    rowlength=round(rowlength*nrow)
+    collength=round(collength*ncol)
+    start_row=int(random.random()*nrow)
+    start_col=int(random.random()*ncol)
+    for row in np.arange(0,rowlength):
+        for col in np.arange(0,collength):
+            miss_data.iloc[(start_row+row)%nrow,(start_col+col)%ncol] = np.nan
     return miss_data
 def readDataAndSplit(path):
     csvdata = pd.read_excel(path,header=None)
@@ -68,3 +136,12 @@ def readDataAndSplit(path):
             misss_row.append(ind)
             continue
     return np.array(data,dtype=float),list(set(misss_row)),name
+
+def check_missrate(data):
+    missCount = 0
+    for i in range(len(data)):
+        for j in range(len(data[i])):
+            if np.isnan(data[i][j]):
+                missCount += 1
+    missRate = missCount / (len(data) * len(data[0]))
+    return missRate
